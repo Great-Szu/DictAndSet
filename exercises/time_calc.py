1bba5970195d9cf0a35913be090a7c8ac5aabc94
Great-Szu <mateusz.chol.net@outlook.com>
2025-05-18 19:00:01 +0200

    Day 13, Difference between .remove and .discard

--- exercises/time_calc.py
+++ exercises/time_calc.py
@@ -8,7 +8,7 @@ def time_calculator(hours: list[int], minutes: list[int]) -> float:
     total_minutes = sum(hours) * 60 + sum(minutes)
     return total_minutes / 60
 
-godzinki = [2, 4, 4, 3, 5]
-minutki = [14, 35, 30, 13, 45, 58]
+h = []
+m = [556]
 
-print(time_calculator(godzinki, minutki))
+print(time_calculator(h, m))
