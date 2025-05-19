661a792b2de37681fb68137cf52ae099b0797af8
Great-Szu <mateusz.chol.net@outlook.com>
2025-05-19 19:44:42 +0200

    Day 14, .pop method & combining sets

---
+++ prescription_processing.py
@@ -0,0 +1,9 @@
+from prescription_data import patients
+
+trail_patients = {"Denise", "Eddie", "Frank", "Georgia", "Kenny"}
+
+while trail_patients:
+    patient = trail_patients.pop()
+    print(patient, end=" : ")
+    prescription = patients[patient]
+    print(prescription)
